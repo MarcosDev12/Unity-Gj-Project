d865a88dd707b926407c6a6543f56ade64825362
MarcosDev <virgilio.mvrve@gmail.com>
2023-10-21 17:35:10 -0300

    Update 1.5 - Rotação com o mouse
    Agora é possivel movimentar a terra com o mouse

---
+++ Assets/Scripts/CameraMoviment.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMoviment : MonoBehaviour
+{
+    public Transform target; // O objeto ao redor do qual a câmera vai girar
+    public float rotationSpeed = 5.0f; // Velocidade de rotação
+    private Vector3 offset;
+
+    private void Start()
+    {
+        offset = transform.position - target.position; // Calcula a movimentção da câmera em relação ao objeto
+    }
+
+    void Update()
+    {
+        // Verifique se o botão direito do mouse está pressionado
+        if (Input.GetMouseButton(1))
+        {
+            // Captura as mudanças no movimento do mouse
+            float mouseX = Input.GetAxis("Mouse X") * rotationSpeed;
+
+            // Rotaciona o objeto alvo
+            target.Rotate(Vector3.up, mouseX);
+
+            // Mantenha a posição da câmera em relação ao objeto
+            transform.position = target.position + offset;
+        }
+    }
+}
--- Assets/Scripts/PlanetMove.cs
+++ Assets/Scripts/PlanetMove.cs
@@ -6,11 +6,6 @@ public class PlanetMove : MonoBehaviour
 {
     public Transform transform;
     public float spdEarth;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
